9064d12dbbef86141ca9d8388e21dabe1c2e3a4b
ainamanoa <ainaarajoel@gmail.com>
2026-06-23 16:20:43 +0300

    Sprint2-fin

--- src/mg/itu/app/servlet/FrontControllerServlet.java
+++ src/mg/itu/app/servlet/FrontControllerServlet.java
@@ -6,9 +6,15 @@ import jakarta.servlet.http.*;
 import mg.itu.app.tools.Utils;
 import java.util.List;
 import mg.itu.app.annotation.Controller;
+import mg.itu.app.tools.URLInfo;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import mg.itu.app.annotation.URLMapping;
 
 public class FrontControllerServlet extends HttpServlet {
     private List<String> annotatedClasses;
+    private Map<String, URLInfo> urlMappings = new HashMap<>();
 
     @Override
     public void init() throws ServletException {
@@ -18,6 +24,7 @@ public class FrontControllerServlet extends HttpServlet {
 
         try {
             annotatedClasses = Utils.getClassesContainingAnnotation(packageNames, Controller.class);
+            Utils.getURLMappings(packageNames, Controller.class, URLMapping.class, urlMappings);
         } catch (Exception e) {
             throw new ServletException("Error initializing annotated classes", e);
         }
@@ -40,15 +47,41 @@ public class FrontControllerServlet extends HttpServlet {
         String context = request.getContextPath();
         String answer = path.substring(context.length());
 
+        out.println("SPRINT 0: " + answer);
+
+        String sprint1="";
+
         if (annotatedClasses != null && !annotatedClasses.isEmpty()) {
-            answer += "\nAnnotated classes:\n";
+            sprint1 += "\nSPRINT1: ANNOTATED CLASSES:\n";
             for (String className : annotatedClasses) {
-                answer += className + "\n";
+                sprint1 += className + "\n";
             }
         } else {
-            answer += "\nNo annotated classes found.";
+            sprint1 += "\nNo annotated classes found.";
         }
 
-        out.println(answer);
+        boolean isURLMappingsPopulated = false;
+
+        for (Map.Entry<String, URLInfo> entry : urlMappings.entrySet()) {
+
+            if (entry.getKey().equals(answer)) {
+
+                isURLMappingsPopulated = true;
+                URLInfo urlInfo = entry.getValue();
+                sprint1 += "\nSPRINT2:\n";  
+                sprint1 += "\n Mapped to class: " + urlInfo.getClazz().getName() + ", method: " + urlInfo.getMethod().getName();
+                break;
+            }
+        }
+
+        if (!isURLMappingsPopulated) {
+            sprint1 += "\nSPRINT2:\n";  
+            for (Map.Entry<String, URLInfo> entry : urlMappings.entrySet()) {
+                URLInfo urlInfo = entry.getValue();
+                sprint1 += "\n Mapped to class: " + urlInfo.getClazz().getName() + ", method: " + urlInfo.getMethod().getName() + ", URL: " + entry.getKey();
+            }
+        }
+
+        out.println(sprint1);
     }
 }
